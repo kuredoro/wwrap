9c8433b224160c3a009b31b7d9b6571f01877767
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-11-19 15:18:27 +0300

    testing: CharWrapper tests switched to TDT.

--- char_wrapper_test.go
+++ char_wrapper_test.go
@@ -2,47 +2,38 @@ package wwrap_test
 
 import (
     "io"
+    "fmt"
     "testing"
 
     "github.com/kuredoro/wwrap"
 )
 
 func TestCharWrapper(t *testing.T) {
-    t.Run("line that fits the width", func(t *testing.T) {
-        text := "あいうえお"
-
-        cw := &wwrap.CharWrapper{Width: 10}
-        wwrap.AssertWriteString(t, cw, text)
-
-        b, err := io.ReadAll(cw)
-        if err != nil {
-            t.Errorf("got error, want none. Error: %v", err)
-        }
-
-        want := "あいうえお"
-        got := string(b)
-
-        if got != want {
-            t.Errorf("got %q, want %q", got, want)
-        }
-    })
-
-    /*
-    text := "今日はいい天気ですね。"
-
-    cw := &wwrap.CharWrapper{Width: 10}
-    wwrap.AssertWriteString(t, cw, text)
-
-    b, err := io.ReadAll(cw)
-    if err != nil {
-        t.Errorf("got error, want none. Error: %v", err)
+    tests := []struct{
+        width uint
+        in string
+        want string
+    }{
+        {10, "あいうえお", "あいうえお"},
+        // {10, "今日はいい天気ですね。", "今日はいい\n天気ですね\n。"},
     }
 
-    want := "今日はいい\n天気ですね\n。"
-    got := string(b)
+    for _, test := range tests {
+        name := fmt.Sprintf("crop %s to %d columns", test.in, test.width)
+        t.Run(name, func(t *testing.T) {
+            cw := &wwrap.CharWrapper{Width: test.width}
+            wwrap.AssertWriteString(t, cw, test.in)
 
-    if got != want {
-        t.Errorf("got %q, want %q", got, want)
+            b, err := io.ReadAll(cw)
+            if err != nil {
+                t.Errorf("got error, want none. Error: %v", err)
+            }
+
+            got := string(b)
+
+            if got != test.want {
+                t.Errorf("got %q, want %q", got, test.want)
+            }
+        })
     }
-    */
 }
